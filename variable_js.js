ffd03fadee480654029b84a6e7c4bc4574ac11f5
VaishnaviBorkar <borkarvaishnavi85@gmail.com>
2021-07-21 19:13:40 +0530

    js codes

---
+++ variable_js.js
@@ -0,0 +1,44 @@
+//for testing
+console.log("js working properly");
+//display alert,warning error msg
+//alert("method used to display alert text with yes option")
+console.error("error user to display error msg with cross icon in red")
+console.warn("display warning text with warnning icon in yellow.")
+// Variables :variable name=value
+//Data types =Number,String,Boolean,null,Indefiend (all objects)
+// Non Premetive Datatypes:Array,object arrayofobject json.
+p=10  //number
+q='string1' //String
+r=null
+s=10.50           //number
+t=5677.999999999999999999999999999999999999999   //number
+u=77777777777777777777777777777777777777777     //number
+v=true   //boolean
+w=undefined
+console.log(p)
+console.log(q)
+console.log(r)
+console.log(s)
+console.log(t)
+console.log(u)
+console.log(v)
+console.log(w)
+//Declaration of variables :var,let,const
+//1.var is global in nature 
+//2.let is local (Es6 keyword)
+//3.const is local & unchangable (Es6 keyword)
+var variable=55
+console.log(variable)
+variable=102
+console.log(`${variable}changable`)
+let variable_2='vaishnavi'
+console.log(variable_2)
+variable_2='tishaa'
+console.log(`${variable_2}changable`)
+const variable_3=67
+console.log(variable_3)
+// variable_3=56 cant change costant variable
+const str = 'This is a String'
+// Find the length of the String
+ console.log(str.length);
+
