57c5e6d515b9b40fe11213d7b1691e26d992c3a7
VaishnaviBorkar <borkarvaishnavi85@gmail.com>
2021-08-09 20:25:58 +0530

    todays practice

---
+++ practice js/arrays.js
@@ -0,0 +1,6 @@
+//pint the pizzaToppings array on terminal
+const pizzaToppings=['tomato sauce','cheese','pepperoni']
+console.log(pizzaToppings);
+// for(let i=0;i<=pizzaToppings.length;i++){
+//     console.log(pizzaToppings[i]);
+// }
---
+++ practice js/object-keys.js
@@ -0,0 +1,9 @@
+const car={
+    make:'Honda',
+    model:'Accord',
+    year:2020
+}
+const keys=Object.keys(car);
+console.log(keys);
+//  using the Object.keys() prototype method.when
+//  you need to list the keys of an object.
